a2d377429f8984bc3d63501d307cf7c950f8cc99
Scott Sutton <scosut@hotmail.com>
2021-02-09 12:09:55 -0800

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
@@ -47,9 +48,10 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path='/home' component={HomePage} />
+          <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
           <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
           <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-          <Route path='/contactus' component={Contact} />
+          <Route exact path='/contactus' component={Contact} />
           <Redirect to='/home' />
         </Switch>
         <Footer />
