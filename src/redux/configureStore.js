45e92450a75758a9652c54d70c7f04d7d07667be
Scott Sutton <scosut@hotmail.com>
2021-02-17 16:08:23 -0800

    Redux Thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,17 +1,21 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import { Campsites } from './campsites';
 import { Comments } from './comments';
 import { Partners } from './partners';
 import { Promotions } from './promotions';
 
 export const ConfigureStore = () => {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   const store = createStore(
     combineReducers({
       campsites: Campsites,
       comments: Comments,
       partners: Partners,
       promotions: Promotions
-    })
+    }),
+    composeEnhancers(applyMiddleware(thunk, logger))
   );
 
   return store;
